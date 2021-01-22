736a536d9b5c00a14a84e9fd538ce7c3c3f558e0
Wizard2D <70329712+Wizard2D@users.noreply.github.com>
2021-01-22 12:27:50 +0200

    CS normal version
    The normal version, new soon.

---
+++ CDullCS/CDullCS/AST.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDullCS
+{
+    class Node
+    {
+        public string val;
+        public int value;
+
+        public Node(string valS, int valueS)
+        {
+            val = valS;
+            value = valueS;
+        }
+    }
+    class Root
+    {
+        public string name;
+        public List<Node> nodes = new List<Node>() { };
+        public void addNode(string val, int value)
+        {
+            nodes.Add(new Node(val, value));
+        }
+    }
+
+    class AST
+    {
+        public List<Root> roots = new List<Root>(){};
+
+        public Root addRoot(string name)
+        {
+            Root newRoot = new Root();
+            newRoot.name = name;
+            roots.Add(newRoot);
+            return newRoot;
+        }
+    }
+}
